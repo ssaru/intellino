4d63aee035ffbbb7ada081f919970cb163b6f66f
Martin Hwang <dhhwang89@gmail.com>
2020-03-03 17:03:50 +0900

    feat: update theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,7 +84,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'sphinx_rtd_theme'
 #html_theme = 'pytorch_sphinx_theme'
 html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
 
